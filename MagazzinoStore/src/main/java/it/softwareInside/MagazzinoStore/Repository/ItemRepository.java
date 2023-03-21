6b730b1b5c0750f710d648e9d0cd9d7e7dcc847c
Ales-sandroo <125959503+Ales-sandroo@users.noreply.github.com>
2023-03-21 09:52:38 +0100

    Delete MagazzinoStore directory

--- MagazzinoStore/src/main/java/it/softwareInside/MagazzinoStore/Repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package it.softwareInside.MagazzinoStore.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.softwareInside.MagazzinoStore.Models.Item;
-
-public interface ItemRepository extends CrudRepository<Item, Integer> {
-
-}
